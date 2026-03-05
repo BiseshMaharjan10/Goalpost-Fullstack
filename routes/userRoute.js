77f04705f3e4e94ef553d268393f5945e0d09aa8
BiseshMaharjan10 <biseshmhrz01@gmail.com>
2026-03-05 08:55:28 +0545

    added routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const upload = multer();
 const uploadProductImages = require("../helpers/multer");
 
-const {registerUser, loginUser, getallUser,getMe, updateImage} = require('../controllers/userController');
+const {registerUser, loginUser, getallUser,getMe, updateImage,forgotPassword,resetPassword} = require('../controllers/userController');
 const verifyEmail = require('../helpers/verifyEmail');
 const authGuard = require("../helpers/authGuard");
 const isAdmin = require('../helpers/isAdmin');
@@ -14,5 +14,7 @@ router.get("/getAllUser",authGuard,isAdmin,getallUser);
 router.post("/loginUser",loginUser);
 router.put("/updateImage",authGuard,uploadProductImages,updateImage);
 router.get("/getme",authGuard,getMe);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
